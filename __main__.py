b34b3c27396f738c09885e1f94cde4164ccc4431
Shamus Coughlin <shamus.coughlinhorton@student.sl.on.ca>
2020-11-25 18:21:46 +0000

    update gui every 500ms

--- __main__.py
+++ __main__.py
@@ -1,13 +1,34 @@
 import tkinter as tk
+import time
+
+def returnWpm():
+    #Calculation to measure wpm here
+    return time.strftime("%Y%m%dT%H%M%SZ", time.gmtime())
 
 #Create Gui Window
 window = tk.Tk()
 window.configure(bg='black')
 wordlist = tk.Label(text="Wouldn't it be lovely to enjoy a week soaking up the culture?")
-wordlist.config(font=("Courier", 18))
+prompt = tk.Label(text="Start typing to begin!")
+prompt.config(font=("Courier", 12))
+wordlist.config(font=("Courier", 16))
+wpm = tk.Label(font=('Helvetica', 24))
+
+#Update every 500 milliseconds
+def updatewindow():
+  now = tk.StringVar()
+  wpm["textvariable"] = now
+  now.set(returnWpm())
+  #Everything before this line gets repeated
+  window.after(500, updatewindow)
+
 #Text Input
-textinput = tk.Entry(width=80)
+textinput = tk.Entry(width=70)
+
 #Pack labels
-wordlist.pack(padx=120,pady=20)
-textinput.pack(padx=120,pady=40)
+wordlist.pack(padx=100,pady=20)
+prompt.pack(padx=100,pady=10)
+wpm.pack(padx=100,pady=10)
+textinput.pack(padx=100,pady=40)
+updatewindow()
 window.mainloop()
